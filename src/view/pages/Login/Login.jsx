4bd2933455b1bea949a0d9e8ce954b84dec413f3
Santy <santiagomoralestutistar123@gmail.com>
2023-11-05 16:56:12 -0500

    ya se puede iniciar sesión

--- src/view/pages/Login/Login.jsx
+++ src/view/pages/Login/Login.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import './Login.css'
 
 export default function Login () {
+  const navigate = useNavigate()
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+    fetch('https://birsbane-numbat-zjcf.1.us-1.fl0.io/api/user/auth', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: event.target.elements.email.value,
+        password: event.target.elements.password.value
+      })
+    }).then((response) => {
+      if (response.ok) return response.json()
+      throw new Error('Error al autenticar el usuario')
+    }).then(() => {
+      navigate('/app')
+    }).catch(error => {
+      console.log('Error en navegacion' + error)
+    })
+  }
+
   return (
     <div className='login'>
       <main className='caja-form'>
-        <form className='form'>
+        <form className='form' onSubmit={handleLogin}>
           <h1 className='form__title'>Ingreso</h1>
 
           <div className='form__campos'>
@@ -18,7 +41,7 @@ export default function Login () {
             <input className='campo' name='password' type='password' placeholder='contraseña' />
           </div>
 
-          <button className='btn'>Entrar</button>
+          <button type='submit' className='btn'>Entrar</button>
 
           <div className='text'>
             <p className='text__p'>¿Aún no tienes una cuenta? &nbsp;
