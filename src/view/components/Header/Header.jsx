dc27cc09f8a54f151de9d17b74d2aa2119fea8bd
Santy <santiagomoralestutistar123@gmail.com>
2023-11-11 09:18:07 -0500

    creamos carpeta de svg, hicimos el npm run build (añadiendo ese dist) y ahora cada que salgas del app y vuelvas a la pantalla de inicio tendrás que volver a iniciar sesion

--- src/view/components/Header/Header.jsx
+++ src/view/components/Header/Header.jsx
@@ -4,17 +4,21 @@ import { Link } from 'react-router-dom'
 export default function Header ({ toggleMode, theme, mode }) {
   const user = JSON.parse(globalThis.localStorage.getItem('USER'))
 
+  const deleteUser = () => {
+    globalThis.localStorage.removeItem('USER')
+  }
+
   return (
     <header className='header' theme={theme}>
       <div className='header__text'>
-        <Link to='/inicio' className='tittle'>noteX</Link>
+        <Link to='/inicio' className='tittle' onClick={deleteUser}>noteX</Link>
         <p className='welcome'>Bienvenid@, <span className='user-name'>{user.firstName}</span>
-          <img className='img-user' src={`../../../assets/user-${mode ? 'black' : 'white'}.svg`} alt='plus' width='25px' />
+          <img className='img-user' src={`../../../assets/svg/user-${mode ? 'black' : 'white'}.svg`} alt='plus' width='25px' />
         </p>
 
       </div>
       <div className='mode'>
-        <img className='sun' src={`../../../assets/svg${mode ? 'Moon' : 'Sun'}.svg`} alt='' width='35px' onClick={toggleMode} />
+        <img className='sun' src={`../../../assets/svg/svg${mode ? 'Moon' : 'Sun'}.svg`} alt='' width='35px' onClick={toggleMode} />
       </div>
     </header>
   )
--- src/view/pages/Application/Application.jsx
+++ src/view/pages/Application/Application.jsx
@@ -64,7 +64,7 @@ export default function Application () {
           </div>
         </main>
         <div className='img-div' onClick={toggleOpen}>
-          <img className='img-plus' src={`../../../assets/plus-${mode ? 'black' : 'white'}.svg`} alt='plus' width='25px' />
+          <img className='img-plus' src={`../../../assets/svg/plus-${mode ? 'black' : 'white'}.svg`} alt='plus' width='25px' />
         </div>
       </div>
       {isOpen && <NewNote toggleOpen={toggleOpen} handleCreate={handleCreate} />}
